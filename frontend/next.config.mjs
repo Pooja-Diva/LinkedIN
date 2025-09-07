2f49fdd4cdccc4a786f22b9a516e0ea47a58f6a8
Pooja <py028203@gmail.com>
2025-09-07 11:17:00 +0530

    Commit for LinkedINClone

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   webpack: (config) => {
     return config; // just ensures Webpack is used
   },
+
+   async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://linkedinclone-zuc5.onrender.com/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
